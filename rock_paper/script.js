6c5d3b1ab55714c96263cedcb76db06011448f60
Hubert Wojnowski <hubertwojnowski0@gmail.com>
2023-09-19 22:37:04 +0200

    Did some first attempts

--- rock_paper/script.js
+++ rock_paper/script.js
@@ -1,1 +1,33 @@
-console.log("Hello")
+
+const randomNumber = Math.floor(Math.random() * 3) + 1;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function playRound(playerSelection, computerSelection) {
+    // your code here!
+  }
+   
+  const playerSelection = "rock";
+  const computerSelection = getComputerChoice();
+  console.log(playRound(playerSelection, computerSelection)
